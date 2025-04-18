606a88c678ed772645b1838d12fdc928c600e79a
ROHAN BHENDEKAR <161940056+rohanbhendekar21@users.noreply.github.com>
2025-04-18 12:49:17 +0530

    Add files via upload

---
+++ portfolio2 (final)/README.md
@@ -0,0 +1,2 @@
+# Rohan Bhendekar Portfolio
+This is my portfolio website built using React.
---
+++ portfolio2 (final)/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold text-white">Rohan Bhendekar</h1>
+      <p className="text-gray-400">Welcome to my Portfolio Website</p>
+    </div>
+  );
+}
+
+export default App;
